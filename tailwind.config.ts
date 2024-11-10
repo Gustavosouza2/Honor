f314b44b0e1f724496d78721a316b4681692f109
GustavoSouza2 <gustavoleonsouza@gmail.com>
2024-11-09 22:42:44 -0300

    build:adding lib for tailwind animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import tailwindcssAnimated from "tailwindcss-animated";
 
 export default {
   content: [
@@ -14,5 +15,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [tailwindcssAnimated],
 } satisfies Config;
